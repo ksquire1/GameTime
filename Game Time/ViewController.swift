14677b6840709e180b1649fd46717e82b5be806d
Keith Squire <ksquire1@me.com>
2016-01-27 16:20:16 +1100

    Initial Commit

---
+++ Game Time/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Game Time
+//
+//  Created by Keith Squire on 2/08/2015.
+//  Copyright (c) 2015 Keith Squire Inc. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
